5a88381cf79b35a2ee2c962883f983096125a546
Daria Aleksandrova <106164543+AleksandrovaDaria@users.noreply.github.com>
2023-02-18 03:49:09 +0100

    AI lite level
    AI lite level

--- ai_lite.py
+++ ai_lite.py
@@ -4,6 +4,7 @@ def  get_random_ai_coordinates(game_progress):
 
     coordinate_1 = random.randint(0,2)
     coordinate_2 = random.randint(0,2)
+    
     if game_progress[coordinate_1][coordinate_2] == False: return (coordinate_1,coordinate_2)
     else: 
         get_random_ai_coordinates(game_progress)
